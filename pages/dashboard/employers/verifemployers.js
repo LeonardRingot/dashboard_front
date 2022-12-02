6c348b335cd2a39a89a5623a5e836d4d102f5413
LeonardRingot <leonardringot6@gmail.com>
2022-12-02 19:42:29 +0100

    test form update

--- pages/dashboard/employers/verifemployers.js
+++ pages/dashboard/employers/verifemployers.js
@@ -7,7 +7,7 @@ import styles from '../../../styles/Home.module.css'
 export default function Verifemployers(){
     const router = useRouter()
 const {id} = router.query
-
+const [isActif, setIsActif] = useState([]);
 
 const [verifEmployers, setverifEmployers] = useState({
     siret: '',
@@ -19,42 +19,33 @@ const [verifEmployers, setverifEmployers] = useState({
               zipCode: ''
         },
       email: '',
-      phone: ''
+      phone: '',
+      isActif:'',
     },
     periods:'',
 })
-// const handleChange = (e) => 
-//     {
-//       const value = e.target.value;
-//       if (e.target.name == "periods")
-//       {
-//         let myperiod = period.find((period) => period == value)
-//          if (myperiod == null)
-//          {
-//           period.push(value)
-//          } else 
-//          {
-//           let tabPeriod = []
-//           period.map((period) => {
-//             period != value? tabPeriod.push(period): ''
-//           })
-//           setPeriod(tabPeriod)
-//          }
-//       }
-      
-//       setverifEmployers({
-//         ...verifEmployers,
-//         User:{
-//           ...verifEmployers.User,
-//               [e.target.name]: value,
-//               Localisation: {
-//                 ...verifEmployers.User.Localisation,
-//                 [e.target.name]: value,
-//               },
-//         },
-//         [e.target.name]: value
-//       });
-//     }
+ const handleChange = (e) => 
+ {
+
+ if(e.target.name == "isActif")
+ {
+   console.log(verifEmployers.User.isActif)
+ }
+ setverifEmployers({
+     ...verifEmployers,
+       User:
+       {
+         ...verifEmployers.User,
+         [e.target.name]: value,
+
+           Localisation: {
+             ...verifEmployers.User.Localisation,
+             [e.target.name]: value,
+           },
+       },
+     [e.target.name]: value
+   });
+}
     useEffect(() => {
         const fetchSomethingById = async () => {
           ServiceAPI.requeteGetEmployerById(id)
@@ -68,19 +59,19 @@ const [verifEmployers, setverifEmployers] = useState({
         fetchSomethingById()
       }, [id])
 
-    //   const ModifierStatutEmployeur = (e) => {
-    //     e.preventDefault()
-    //     ServiceAPI.requeteVerifEmployeur(id, isActif).then(response => {
-    //         if(response.status == 201){
-    //           setIsOk('User mis a jour');
+      const ModifierStatutEmployeur = (e) => {
+        e.preventDefault()
+        ServiceAPI.requeteVerifEmployeur(id, isActif).then(response => {
+            if(response.status == 201){
+              setIsOk('User mis a jour');
               
-    //         } else {
-    //           setIsOk('User mis a jour');
-    //         }
-    //       }).catch(function(error){
-    //         console.log(error);
-    //     }); 
-    // }
+            } else {
+              setIsOk('User mis a jour');
+            }
+          }).catch(function(error){
+            console.log(error);
+        }); 
+    }
     return (
     <>
     <Head>
@@ -96,11 +87,11 @@ const [verifEmployers, setverifEmployers] = useState({
         <li> Adresse:{verifEmployers.User.Localisation.address}</li>
         <li> Ville:{verifEmployers.User.Localisation.city}</li>
         <li> zipCode:{verifEmployers.User.Localisation.zipCode}</li>
-        <li> Périodes:{verifEmployers.periods}</li>
+        {/* <li> Périodes:{verifEmployers.periods}</li> */}
        
       </ul>
-      {/* <form id="register_form"  onSubmit={ModifierStatutEmployeur} action='' method="post" >
-      {/* <fieldset name='isActif'>
+      <form id="register_form"  onSubmit={ModifierStatutEmployeur} action='' method="post" >
+      <fieldset name='isActif'>
             <div class="form-check"   >
                 <input   class="form-check-input"   onChange={handleChange}  value="true"  defaultChecked={verifEmployers.User.isActif} type="radio" name="isActif" id="true"  />
                 <label class="form-check-label" htmlFor="true">✅</label>
@@ -110,8 +101,9 @@ const [verifEmployers, setverifEmployers] = useState({
                 <label class="form-check-label" htmlFor="false">❌</label>
             </div>
             </fieldset>
-            <input  value="Envoyer" class="btn btn-success" type="submit"/> 
-      </form> */} 
+            <input  value="Envoyer" class="btn btn-success" type="submit"/>
+      </form>
+      
     </>
     )
 }
