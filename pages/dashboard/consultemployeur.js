b262b2ca802724d3a0442cd8fb1de13208f54393
LeonardRingot <leonardringot6@gmail.com>
2023-01-05 09:16:47 +0100

    research by zipCode

--- pages/dashboard/consultemployeur.js
+++ pages/dashboard/consultemployeur.js
@@ -9,7 +9,7 @@ const getFilteredItems = (query, items) =>{
     if(!query){
         return items;
     }
-    return items.filter(items =>items.User.email.includes(query))
+    return items.filter(items =>items.User.Localisation.zipCode.includes(query))
 }
 export default function Consultemployeur()
 {
@@ -88,7 +88,7 @@ export default function Consultemployeur()
        <Navbar>
                <div  > 
         <Link href="./employers/createemployer"><a class="btn btn-primary">Creer un employeur</a></Link>
-        <label>Rechercher par adresse mail:</label> <input type="text" onChange={(e) => setQuery(e.target.value)}></input>
+        <label>Rechercher par Code Postal:</label> <input type="text" onChange={(e) => setQuery(e.target.value)}></input>
     <table class="table table-hover table-dark">
         <thead  >
             <tr >
